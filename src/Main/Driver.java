38c9d1e6dc7cf6872a4e91e93c5e2527a3c18a4c
Will Kuglen <wkuglen@gmail.com>
2014-03-16 19:37:32 -0500

    basic setup

---
+++ src/Main/Driver.java
@@ -0,0 +1,20 @@
+package Main;
+
+
+public class Driver {
+    public static void main(String args[])
+    {
+        System.out.println("Version 0.1");
+        System.out.println("TextAdventure");
+        System.out.println("(c) Copyright 2014. I can't enforce copyright");
+
+        System.out.println("");
+        System.out.println("========= =   = =========");
+        System.out.println("    =      = =      =    ");
+        System.out.println("    =       =       =    ");
+        System.out.println("    =      = =      =    ");
+        System.out.println("    =     =   =     =    ");
+
+        
+    }
+}
